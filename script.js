4c9cdd84e0cad58bb798070498762e4d6e7edbe2
Dung Hoang <hoangdung.200298@gmail.com>
2020-11-27 01:04:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+function home(){
+}
+function products(){
+}
+function initialize(){
+	document.getElementById("menu-item").style.maxHeight = "0px";
+}
+function initializeProductDetails(){
+	document.getElementById("menu-item").style.maxHeight = "0px";
+
+	var ProductImg = document.getElementById("ProductImg");
+	var SmallImg = document.getElementsByClassName("small-img");
+	SmallImg[0].onclick = function(){
+		ProductImg.src = SmallImg[0].src;
+	}
+	SmallImg[1].onclick = function(){
+		ProductImg.src = SmallImg[1].src;
+	}
+	SmallImg[2].onclick = function(){
+		ProductImg.src = SmallImg[2].src;
+	}
+	SmallImg[3].onclick = function(){
+		ProductImg.src = SmallImg[3].src;
+	}
+}
+function menutoggle(){
+	var height = document.getElementById("menu-item").style.maxHeight;
+	if(height == "0px"){
+		document.getElementById("menu-item").style.maxHeight = "200px";
+	}
+	else{
+		document.getElementById("menu-item").style.maxHeight = "0px";
+	}
+}
+function viewCart(){
+	alert('viewCart');
+}
+
+function product(){
+	
+}
+/*
+
+*/
